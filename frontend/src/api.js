c0db10fb7ca3931fa2999450ac507428447f51bd
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-01-20 15:11:07 +0530

    fixed some bugs

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,13 +2,16 @@ export async function clipBoardAPI(prompt) {
   console.log(prompt);
   const {
     result: [imageURL],
-  } = await fetch(`/api/convert`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ prompt }),
-  }).then((res) => res.json());
+  } = await fetch(
+    `https://text-to-image-ai-app-backend.vercel.app/api/convert`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    }
+  ).then((res) => res.json());
   console.log("Hello");
   return imageURL;
 }
